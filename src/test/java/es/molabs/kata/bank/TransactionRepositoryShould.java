8f741bf793cfb658ac53bf77c382c68066a1d128
Luis Moral Guerrero <luis.moral@gmail.com>
2018-06-29 17:57:48 +0200

    Refactoring.

--- src/test/java/es/molabs/kata/bank/TransactionRepositoryShould.java
+++ src/test/java/es/molabs/kata/bank/TransactionRepositoryShould.java
@@ -9,14 +9,14 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
-public class TransactionsShould
+public class TransactionRepositoryShould
 {
 	@Test
 	public void calculate_final_balance()
 	{
-		TransactionRepository transactions = new TransactionRepository();
-		transactions.addTransaction(new Transaction(new Date().toString(), 100));
+		TransactionRepository transactionRepository = new TransactionRepository();
+		transactionRepository.addTransaction(new Transaction(new Date().toString(), 100));
 		
-		Assert.assertThat(transactions.getFinalBalance(), Is.is(100));
+		Assert.assertThat(transactionRepository.getFinalBalance(), Is.is(100));
 	}
 }
